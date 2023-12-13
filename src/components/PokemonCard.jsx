f43b4243e28057a7831d72e36c641ef94250bbdc
Slavica474 <147341887+Slavica474@users.noreply.github.com>
2023-12-13 16:03:01 +0100

    props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,23 +1,11 @@
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-function PokemonCard() {
-  const pokemon = pokemonList[0]
-
+function PokemonCard({pokemon}) {
     return (
         <figure>
           {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
             <figcaption>{pokemon.name}</figcaption>
         </figure>
     )
+   
   }
   
 
